a2edfc6394cef1917af80c4d88c36766cc308686
marceljouvenaz <44669735+marceljouvenaz@users.noreply.github.com>
2018-11-19 11:44:45 +0100

    Update simple6.php

--- simple6.php
+++ simple6.php
@@ -1,1 +1,26 @@
+<?php
+//vraagt om getal en telt er naar toe
+//input
+echo "geef een getal: ";
+$input = trim(fgets(STDIN));
+echo "type s for sum, or p for product: ";
+$task = trim(fgets(STDIN));
+//bepaal bewerking
+if($task == p){
+  $answer = 0;
+  for ($i=0 ; $i <= $input ; $i++ ) { 
+	$answer += $i;
+}
+}elseif($task == s){
+  $answer =1;
+  for ($i=0 ; $i <= $input ; $i++ ) { 
+	$answer *= $i;
+}
+}else{
+  $answer = 'only p(roduct) and s(um) are defined acytions';
+}
 
+//calc sum
+
+//view
+echo $answer;
